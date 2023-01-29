38cca22a3559f3cde6aff93fdd755ab01865810d
AYUSH <ayushrajbls@gmail.com>
2023-01-29 18:17:37 +0530

    get and post routes added

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import productRoutes from './routes/products.js'
 
 const app = express();
 
 const PORT = 4000;
 app.use(bodyParser.json());
+app.use('/', productRoutes)
+
 
 app.get("/", (req, res) => {
-  console.log("Test");
+  console.log("Test!");
   res.send('Hello')
 });
+
+
 app.listen(PORT, () =>
   console.log(`Server running on port : http://localhost:${PORT}`)
 );
---
+++ routes/products.js
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const router = express.Router();
+
+
+const products = [{
+    name : "A_Brief_History_Of_Time",
+    category: "Book",
+    genre: "science",},
+    {
+        name : "Black_Holes",
+        category : "Book",
+        genre : "science"
+    }
+]
+
+router.get('/products', (req,res) => {
+    res.send(products)
+});
+
+router.post('/products',(req,res) => {
+   const product = req.body;
+   products.push(product);
+   res.send(`${product.category} of ${product.name} has been added to the database`)
+})
+
+router.post('/',(req,res) => {
+
+})
+
+export default router;
