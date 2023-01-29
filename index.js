4a396bd652eb68fbcc75b72d42f29dae4f443791
AYUSH <ayushrajbls@gmail.com>
2023-01-29 16:02:32 +0530

    Added nodemon

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import express from 'express' ;
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const PORT = 4000;
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  console.log("Test");
+  res.send('Hello')
+});
+app.listen(PORT, () =>
+  console.log(`Server running on port : http://localhost:${PORT}`)
+);
