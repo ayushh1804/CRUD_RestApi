7b20412d691c47bf5f6d626e1c250a0f2913515d
AYUSH <ayushrajbls@gmail.com>
2023-01-29 22:42:28 +0530

    Added routes

--- routes/products.js
+++ routes/products.js
@@ -1,31 +1,31 @@
-import express from 'express'
+import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
+const products = [
+ 
+];
 
-const products = [{
-    name : "A_Brief_History_Of_Time",
-    category: "Book",
-    genre: "science"},
-    {
-        name : "Black_Holes",
-        category : "Book",
-        genre : "science"
-    }
-]
-
-router.get('/products', (req,res) => {
-    res.send(products)
+router.get("/products", (req, res) => {
+  res.send(products);
 });
 
-router.post('/',(req,res) => {
- const product = req.body;
- products.push(product);
-//    products.push(product);
-   res.send(`${product.category} of ${product.name} has been added to the database`)
-})
+router.post("/", (req, res) => {
+  const product = req.body;
 
-router.post('/',(req,res) => {
+  const productWithId = {...product , id: uuidv4()} 
+  products.push(productWithId);
+
+  res.send(
+    `${product.category} of ${product.name} has been added to the database`
+  );
+});
+
+router.get("/products/:id",(req,res) => {
+    const {id} = req.params;
+    const foundProduct = products.find((product) => product.id === id);
+    res.send(foundProduct)
 
 })
 
